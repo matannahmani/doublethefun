5a06bdb29f40f329f41f4e4e2dd51ccc5fefb4ad
Matan Nahmani <matanmatan999new@gmail.com>
2020-02-16 16:31:23 +0200

    Fixed alot of the defualt TODO make routes + seeds , finish event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,29 @@
 class EventsController < ApplicationController
+  before_action :find_event, only: [:edit,:delete,:edit,:event]
+  def index
+    @events = Event.all()
+  end
+
+  def show
+
+  end
+
+  def delete
+    @event.destroy()
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def find_event
+    @event = Event.find(params[:id])
+  end
+
 end
