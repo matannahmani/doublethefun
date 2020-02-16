5560399086dbc738fccd1014fc4810ce381224f3
Eliran Zeitouni <eliran.zeitouni@gmail.com>
2020-02-16 21:26:32 +0200

    added has_many attendances to events controller

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :attendances
   validates  :description, presence: true
   validates  :name, presence: true
   validates  :location, presence: true
