1a5f1674f011606cdcb86feb97a71b70331d88c9
Matan Nahmani <matanmatan999new@gmail.com>
2020-02-16 18:04:56 +0200

    Missing user backend , and checking for owner plus styling for views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :events, only: [:new, :new, :show, :edit]
   root to: 'events#index'
+  delete '/events/:id', to: 'events#destroy', as: :destroy
+  post '/events/create', to: 'events#create', as: :create
+  patch '/events/:id/update', to: 'events#update', as: :update
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
