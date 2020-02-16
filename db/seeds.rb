3a7677caba903562f46f44e0926683ad65da0e21
chris peterson <christonpeterson@gmail.com>
2020-02-16 16:44:50 +0200

    seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Adding seeds to the program"
+
+adam_able = User.new(first_name: "adam", last_name: "able", location: "Tel Aviv", team: "Arsenal")
+adam_adam.save!
+
+bob_blob = User.new(first_name: "bob", last_name: "blob", location: "London", team: "Man U" )
+bob_blob.save!
+
+cris_cringle = User.new(first_name: "cris", last_name: "cringle", location:"Artic", team: "Liverpool")
+cris_cringle.save!
+
+double_dragon = User.new(first_name: "double", last_name: "dragon", location:"Slums", team: "Tottenham")
+double_dragon.save!
+
+earl_easy = User.new(first_name: "earl", last_name:"easy", location:"Hamburg", team: "Chelsey")
+earl_easy.save!
+
+puts "You added 5 seeds!"
+
+
+
