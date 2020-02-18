eb94939ca1b8d5eb82450a5c4d7b850166e5e961
Matan Nahmani <matanmatan999new@gmail.com>
2020-02-18 16:16:41 +0200

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,2 @@
 //= require rails-ujs
 //= require_tree .
-const svgPath = document.querySelectorAll('.path');
-
-const svgText = anime({
-  targets: svgPath,
-  loop: true,
-  direction: 'alternate',
-  strokeDashoffset: [anime.setDashoffset, 0],
-  easing: 'easeInOutSine',
-  duration: 400,
-  delay: (el, i) => { return i * 350 }
-});
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,12 @@
 import "bootstrap";
+const svgPath = document.querySelectorAll('.path');
+
+const svgText = anime({
+  targets: svgPath,
+  loop: true,
+  direction: 'alternate',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 400,
+  delay: (el, i) => { return i * 350 }
+});
