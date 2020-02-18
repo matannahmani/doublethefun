e96bf2b5390e573205b37b51a42a7c2826380ec7
Eliran Zeitouni <eliran.zeitouni@gmail.com>
2020-02-18 16:07:02 +0200

    fixed server boot up

--- config/puma.rb
+++ config/puma.rb
@@ -16,7 +16,8 @@ port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+# REMOVED DUE TO RAILS VERSION
+# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
